3305b24df7820542277ae82c773f950c5881d8ca
TPolan <t.poly@seznam.cz>
2019-02-20 10:26:59 +0100

    migrations complete

--- database/migrations/2019_02_20_092226_create_answers_table.php
+++ database/migrations/2019_02_20_092226_create_answers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateQuestionsTable extends Migration
+class CreateAnswersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateQuestionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('questions', function (Blueprint $table) {
+        Schema::create('answers', function (Blueprint $table) {
             $table->increments('id');
-            $table-unsignedInteger('user_id')->unique();
-            $table->string('title',127);
-            $table->mediumText('text');
+            $table->unsignedInteger('question_id');
+            $table->unsignedInteger('user_id');
+            $table->mediumText('text');            
             $table->timestamps();
-            
         });
     }
 
@@ -30,6 +29,6 @@ class CreateQuestionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('questions');
+        Schema::dropIfExists('answers');
     }
 }
